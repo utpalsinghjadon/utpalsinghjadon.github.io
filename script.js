9bcf955d7f66281fdb44de55364a47562b6c18f0
utpalsinghjadon <futurescientist2805@gmail.com>
2022-05-07 15:28:22 +0530

    formatting

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-const darkModeButton = document.querySelector('.darkmode-button')
-const bodyContainer = document.querySelector('.body-container')
-const aboutmeContainer = document.querySelector('.about-container')
-const navContainer = document.querySelector('.nav-container')
-const projectsContainer = document.querySelector('.projects-container')
-const projectsContainerSmall = document.querySelectorAll('.projects-container-small')
+const darkModeButton = document.querySelector('.darkmode-button');
+const bodyContainer = document.querySelector('.body-container');
+const aboutmeContainer = document.querySelector('.about-container');
+const navContainer = document.querySelector('.nav-container');
+const projectsContainer = document.querySelector('.projects-container');
+const projectsContainerSmall = document.querySelectorAll('.projects-container-small');
+const roles = document.querySelector('.roles');
 
 darkModeButton.addEventListener('click', function () {
     bodyContainer.classList.add('darkmode-body')
@@ -15,3 +16,7 @@ darkModeButton.addEventListener('click', function () {
         i.classList.add('darkmode-body');
     }
 })
+
+// setTimeout(function () {
+//     roles.textContent = 'Front End Designer'
+// }, 2000)
